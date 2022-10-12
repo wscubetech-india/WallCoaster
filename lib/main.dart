2ac158f6b2ca340636853d87aff3ecfcc807c532
raunakpurohit <raunak.wscubetech@gmail.com>
2022-10-12 17:38:17 +0530

    Initial Commit (Basic Home Design )

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Whatsapp'),
+      ),
+      body: ListView.builder(itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/avatar2.png'),
+          ),
+          title: Text('Raman'),
+          subtitle: Text('Hi!'),
+          trailing: CircleAvatar(
+            child: Text('2'),
+          ),
+        );
+      }, itemCount: 10,)
+    );
+  }
+}
